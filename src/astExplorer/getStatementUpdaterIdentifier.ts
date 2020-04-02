aa667bdc6c993108acde6a83a5803ea140e47f55
Mohamad Mohebifar <netfars@gmail.com>
2020-04-02 00:41:10 -0400

    Specify return type for getStatementUpdaterIdentifier

--- src/astExplorer/getStatementUpdaterIdentifier.ts
+++ src/astExplorer/getStatementUpdaterIdentifier.ts
@@ -3,7 +3,7 @@ import * as t from "@babel/types";
 
 export default function getStatementUpdaterIdentifier(
   path: NodePath<t.VariableDeclaration>
-) {
+): string {
   const { node: id } = path.get("declarations")[0].get("id");
   if (t.isIdentifier(id)) {
     return id.name;
@@ -11,5 +11,7 @@ export default function getStatementUpdaterIdentifier(
   if (t.isArrayPattern(id) && t.isIdentifier(id.elements[0])) {
     return id.elements[0].name;
   }
-  throw new Error("Statement updater declarator must be an Identifier or ArrayPattern");
+  throw new Error(
+    "Statement updater declarator must be an Identifier or ArrayPattern"
+  );
 }
